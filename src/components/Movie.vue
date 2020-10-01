599fb5450febc1dc772d0a16b15ac81813b79a64
Stormgren <sbnv93@gmail.com>
2020-10-02 00:03:55 +0200

    Created movie component that shows individual movie posters. Implemented css in assets folder for the styling of whole application

---
+++ src/components/Movie.vue
@@ -0,0 +1,26 @@
+<template>
+ 
+  <img :src="posterImage" :alt="movie.title" />
+
+</template>
+
+<script>
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154'
+export default {
+    name: 'Movie',
+    props: ['movie'],
+    computed: {
+      posterImage: function() {
+        return `${POSTER_PATH}/${this.movie.poster_path}`
+      }
+    }
+}
+</script>
+
+<style scooped>
+img {
+  box-shadow: 0 0 35px black;
+  
+}
+
+</style>
--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,14 +1,22 @@
 <template>
-    <h1>{{ movie.title }}</h1>
+    <ul>
+        <li v-for="movie in movies.results" :key="movie.id"><Movie v-bind:movie="movie"/></li>
+    </ul>
 </template>
 
 <script>
+        import Movie from './Movie.vue'
+    
     export default {
+
         name: 'MoviesList',
         data(){
             return {
-                movie: {}
+                movies: []
             }
+        },
+        components: {
+            Movie
         },
         created: function () {
             this.fetchData()
@@ -16,9 +24,9 @@
         methods: {
             fetchData: async function() {
                 try{
-                let res = await fetch('https://api.themoviedb.org/3/movie/550?api_key=e2ab139be3e5d83d5ebb4792327c1e07')
-                let movie = await res.json();
-                this.movie = movie;
+                let res = await fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e2ab139be3e5d83d5ebb4792327c1e07')
+                let movies = await res.json();
+                this.movies = movies;
  
                 } catch(e) {
                     console.log(e)
@@ -28,5 +36,13 @@
     }
 </script>
 
-<style>
+<style scooped>
+ul {
+    display: grid;
+    list-style: none;
+    padding: 1rem;
+    margin: 0;
+    grid-row-gap: 1rem;
+    grid-template-columns: repeat(6, 1fr);
+}
 </style>
